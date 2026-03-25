1f737ea723121fa825cbdac3663d3472a89325d7
Muhammadbobur22 <muhammadbobur313@gmail.com>
2026-03-25 15:27:05 +0500

    nma gap

---
+++ src/components/favorites/Favorites.jsx
@@ -0,0 +1,133 @@
+import watch from "../../assets/watch.png"
+import "./Favourites.css"
+const Favourites = () => {
+  return (
+    <div>
+      <div className="cards">
+        <div className="card">
+            <div className="card_img">
+                <img src={watch} alt="" />
+            </div>
+            <div className="card_text">
+                <h3>Apple Watch Series 4 </h3>
+                <b>$120.00</b>
+                <p>⭐⭐⭐⭐</p>
+                <button>Edit Project</button>
+            </div>
+        </div>
+
+
+           <div className="card">
+            <div className="card_img">
+                <img src={watch} alt="" />
+            </div>
+            <div className="card_text">
+                <h3>Apple Watch Series 4 </h3>
+                <b>$120.00</b>
+                <p>⭐⭐⭐⭐</p>
+                <button>Edit Project</button>
+            </div>
+        </div>
+
+
+
+           <div className="card">
+            <div className="card_img">
+                <img src={watch} alt="" />
+            </div>
+            <div className="card_text">
+                <h3>Apple Watch Series 4 </h3>
+                <b>$120.00</b>
+                <p>⭐⭐⭐⭐</p>
+                <button>Edit Project</button>
+            </div>
+        </div>
+
+
+
+
+           <div className="card">
+            <div className="card_img">
+                <img src={watch} alt="" />
+            </div>
+            <div className="card_text">
+                <h3>Apple Watch Series 4 </h3>
+                <b>$120.00</b>
+                <p>⭐⭐⭐⭐</p>
+                <button>Edit Project</button>
+            </div>
+        </div>
+
+
+
+
+           <div className="card">
+            <div className="card_img">
+                <img src={watch} alt="" />
+            </div>
+            <div className="card_text">
+                <h3>Apple Watch Series 4 </h3>
+                <b>$120.00</b>
+                <p>⭐⭐⭐⭐</p>
+                <button>Edit Project</button>
+            </div>
+        </div>
+
+
+
+
+           <div className="card">
+            <div className="card_img">
+                <img src={watch} alt="" />
+            </div>
+            <div className="card_text">
+                <h3>Apple Watch Series 4 </h3>
+                <b>$120.00</b>
+                <p>⭐⭐⭐⭐</p>
+                <button>Edit Project</button>
+            </div>
+        </div>
+                <div className="card">
+            <div className="card_img">
+                <img src={watch} alt="" />
+            </div>
+            <div className="card_text">
+                <h3>Apple Watch Series 4 </h3>
+                <b>$120.00</b>
+                <p>⭐⭐⭐⭐</p>
+                <button>Edit Project</button>
+            </div>
+        </div>
+                <div className="card">
+            <div className="card_img">
+                <img src={watch} alt="" />
+            </div>
+            <div className="card_text">
+                <h3>Apple Watch Series 4 </h3>
+                <b>$120.00</b>
+                <p>⭐⭐⭐⭐</p>
+                <button>Edit Project</button>
+            </div>
+        </div>
+                <div className="card">
+            <div className="card_img">
+                <img src={watch} alt="" />
+            </div>
+            <div className="card_text">
+                <h3>Apple Watch Series 4 </h3>
+                <b>$120.00</b>
+                <p>⭐⭐⭐⭐</p>
+                <button>Edit Project</button>
+            </div>
+        </div>
+
+
+
+
+
+      </div>
+    </div>
+  )
+}
+
+export default Favourites
---
+++ src/components/products/Products.jsx
@@ -0,0 +1,146 @@
+import banner from '../../assets/banner.png'
+import watch from "../../assets/watch2.png"
+import './Products.css'
+
+const Products = () => {
+
+  return (
+      <div className="container">
+        <div className="products__section">
+          <h3>Products</h3>
+
+          <div className="banner">
+            <img src={banner} alt="" />
+          </div>
+
+          <div className="cards">
+                <div className="card">
+                    <div className="card_img">
+                        <img src={watch} alt="" />
+                    </div>
+                    <div className="card_text">
+                        <h3>Apple Watch Series 4 </h3>
+                        <b>$120.00</b>
+                        <p>⭐⭐⭐⭐</p>
+                        <button>Edit Project</button>
+                    </div>
+                </div>
+        
+        
+                   <div className="card">
+                    <div className="card_img">
+                        <img src={watch} alt="" />
+                    </div>
+                    <div className="card_text">
+                        <h3>Apple Watch Series 4 </h3>
+                        <b>$120.00</b>
+                        <p>⭐⭐⭐⭐</p>
+                        <button>Edit Project</button>
+                    </div>
+                </div>
+        
+        
+        
+                   <div className="card">
+                    <div className="card_img">
+                        <img src={watch} alt="" />
+                    </div>
+                    <div className="card_text">
+                        <h3>Apple Watch Series 4 </h3>
+                        <b>$120.00</b>
+                        <p>⭐⭐⭐⭐</p>
+                        <button>Edit Project</button>
+                    </div>
+                </div>
+        
+        
+        
+        
+                   <div className="card">
+                    <div className="card_img">
+                        <img src={watch} alt="" />
+                    </div>
+                    <div className="card_text">
+                        <h3>Apple Watch Series 4 </h3>
+                        <b>$120.00</b>
+                        <p>⭐⭐⭐⭐</p>
+                        <button>Edit Project</button>
+                    </div>
+                </div>
+        
+        
+        
+        
+                   <div className="card">
+                    <div className="card_img">
+                        <img src={watch} alt="" />
+                    </div>
+                    <div className="card_text">
+                        <h3>Apple Watch Series 4 </h3>
+                        <b>$120.00</b>
+                        <p>⭐⭐⭐⭐</p>
+                        <button>Edit Project</button>
+                    </div>
+                </div>
+        
+        
+        
+        
+                   <div className="card">
+                    <div className="card_img">
+                        <img src={watch} alt="" />
+                    </div>
+                    <div className="card_text">
+                        <h3>Apple Watch Series 4 </h3>
+                        <b>$120.00</b>
+                        <p>⭐⭐⭐⭐</p>
+                        <button>Edit Project</button>
+                    </div>
+                </div>
+                        <div className="card">
+                    <div className="card_img">
+                        <img src={watch} alt="" />
+                    </div>
+                    <div className="card_text">
+                        <h3>Apple Watch Series 4 </h3>
+                        <b>$120.00</b>
+                        <p>⭐⭐⭐⭐</p>
+                        <button>Edit Project</button>
+                    </div>
+                </div>
+                        <div className="card">
+                    <div className="card_img">
+                        <img src={watch} alt="" />
+                    </div>
+                    <div className="card_text">
+                        <h3>Apple Watch Series 4 </h3>
+                        <b>$120.00</b>
+                        <p>⭐⭐⭐⭐</p>
+                        <button>Edit Project</button>
+                    </div>
+                </div>
+                        <div className="card">
+                    <div className="card_img">
+                        <img src={watch} alt="" />
+                    </div>
+                    <div className="card_text">
+                        <h3>Apple Watch Series 4 </h3>
+                        <b>$120.00</b>
+                        <p>⭐⭐⭐⭐</p>
+                        <button>Edit Project</button>
+                    </div>
+                </div>
+        
+        
+        
+        
+        
+              </div>
+           
+          </div>
+        </div>
+      
+  )
+}
+
+export default Products
