e944004c31ea50b77512a0407ef1ae7a763230f3
Abdulaziz <a81437781@gmail.com>
2026-03-25 17:29:45 +0500

    abdulahad

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
-import Invoice from "./components/invoice/Invoice.jsx";
-import Team from "./components/team/Team.jsx";
 
 const App = () => {
   return (
     <div>
-      <Invoice />
-      <Team />
     </div>
   );
 };
